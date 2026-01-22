02996fa6d9890ad7d7b440b9cf40f0216e7e9e7f
richefeu <vincent.richefeu@univ-grenoble-alpes.fr>
2026-01-22 07:24:18 +0100

    improvements to 'see'

--- mpm/2D/Obstacles/Obstacle.hpp
+++ mpm/2D/Obstacles/Obstacle.hpp
@@ -13,34 +13,35 @@ struct MaterialPoint;
 struct BoundaryForceLaw;
 
 struct Obstacle {
-  int group;
-  double securDist;
-  bool isFree;  // true -> x, y, and rot free; false -> vx and vy imposed, and vrot = 0
+  int group{0};          // group for interaction parameters
+  double securDist{0.0}; // security distance for detecting contact with MPs (seen as a disk)
+  bool isFree{false};    // true -> x, y, and rot free; false -> vx and vy imposed, and vrot = 0
 
-  double mass;
-  double I;
+  double mass{0.0}; // mass is requiered when free motion is allowed
+  double I{0.0};    // Inertia is requiered when rotation free motion is allowed
 
-  vec2r pos;
-  vec2r vel;
-  vec2r acc;
-  vec2r force;
+  vec2r pos;   // position
+  vec2r vel;   // velocity
+  vec2r acc;   // acceleration
+  vec2r force; // resultant force
 
-  double rot;
-  double vrot;
-  double arot;
-  double mom;
-  BoundaryForceLaw* boundaryForceLaw;
+  double rot{0.0};  // angular position
+  double vrot{0.0}; // angular velocity
+  double arot{0.0}; // angular acceleration
+  double mom{0.0};  // resultant moment
+
+  BoundaryForceLaw *boundaryForceLaw;
 
   std::vector<Neighbor> Neighbors; // MP neighbors
 
-  virtual std::string getRegistrationName() = 0;
-  virtual void read(std::istream& is) = 0;
-  virtual void write(std::ostream& os) = 0;
-  virtual void checkProximity(MPMbox& MPM) = 0;
-  virtual int touch(MaterialPoint& MP, double& dn) = 0;
-  virtual void getContactFrame(MaterialPoint& MP, vec2r& n, vec2r& t) = 0;
+  virtual std::string getRegistrationName()                           = 0;
+  virtual void read(std::istream &is)                                 = 0;
+  virtual void write(std::ostream &os)                                = 0;
+  virtual void checkProximity(MPMbox &MPM)                            = 0;
+  virtual int touch(MaterialPoint &MP, double &dn)                    = 0;
+  virtual void getContactFrame(MaterialPoint &MP, vec2r &n, vec2r &t) = 0;
 
-  virtual bool inside(vec2r& x);
+  virtual bool inside(vec2r &x);
 
   Obstacle();
   virtual ~Obstacle();
